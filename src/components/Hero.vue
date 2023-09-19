7d15e4a29b343038f6c25482e0cee8a0cbd326bc
lilskywalkr <ronald.frangulyan@gmail.com>
2023-09-19 21:42:47 +0200

    hero section - added styles

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -21,20 +21,27 @@
 
 <template>
     <div class="hero-container">
-        <div id="hero-text-content-1" :style="{background: '#297'}">
+        <div id="hero-text-content-1">
             <p>Welcome to</p>
             <h1>Thunderbolt<br>Software <span><span class="h-house">H</span>ouse</span></h1>
         </div>
 
-        <div id="hero-text-content-2" :style="{background: '#fff'}">
-            <div class="image" v-bind:style="{ backgroundImage: `url(${imageUrl})` }"></div>
+        <div id="hero-text-content-2">
+            <div class="image" v-bind:style="{ backgroundImage: `url(${imageUrl})` }">
+                <div class="play-button"></div>
+            </div>
         </div>
 
-        <div id="hero-text-content-3" :style="{background: '#245'}">
+        <div id="hero-text-content-3">
             <p>Place where well-crafted <br>software projects are born.</p>
         </div>
 
-        <div id="hero-text-content-4" :style="{background: '#789'}">
+        <div id="hero-text-content-4">
+            <h1><i>Of</i> digital <br>aesthetics</h1>
+            <div class="bottom-text">
+                <p>We produce</p>
+                <p>digital & <br>brand design</p>
+            </div>
         </div>
     </div>
 </template>
@@ -51,14 +58,61 @@
     
         #hero-text-content {
             &-1 {
-                padding: 4vw 0 0 4vw;
+                padding: 4vw 0 0 5vw;
 
-                span {
-                    color: var(--sh-pink);
-                    
-                    .h-house {
-                        display: inline-block;
-                        transform: rotateZ(30deg) translate(-2px, -2px);
+                h1 {
+                    font-size: 5vw;
+                    line-height: 5vw;
+                    span {
+                        color: var(--sh-pink);
+                        
+                        .h-house {
+                            display: inline-block;
+                            transform: rotateZ(30deg) translate(-2px, -2px);
+                        }
+                    }
+                }
+
+                p {
+                    font-size: 1.2vw;
+                }
+            }
+
+            &-2 {
+                display: flex;
+                justify-content: flex-end;
+                padding: 4vw 5vw 0 0;
+            }
+
+            &-3 {
+                padding: 0vw 0 0 5vw;
+
+                p {
+                    font-size: 1.2vw;
+                }
+            }
+        
+            &-4 {
+                display: flex;
+                flex-flow: column nowrap;
+                align-items: end;
+                padding: 0 5vw 0 0;
+
+                h1 {
+                    font-size: 5vw;
+                    line-height: 5vw;
+                }
+
+                .bottom-text {
+                    padding: 3vw 0 0 0;
+                    display: flex;
+                    text-align: left;
+                    font-size: 1vw;
+
+                    p {
+                        font-size: 1.2vw;
+                        display: block;
+                        margin: 0 4.7vw 0 0;
                     }
                 }
             }
@@ -68,8 +122,26 @@
             width: 10vw;
             height: 15vw;
             background-position: center;
-            background-size: cover;
+            background-size: 250%;
             border-radius: 1vw;
+            transition: all .6s cubic-bezier(.23,1,.32,1);
+            position: relative;
+            cursor: pointer;
+
+            &:hover {
+                background-size: 280%;
+            }
+
+            .play-button {
+                width: 3vw;
+                height: 3vw;
+                border-radius: 2vw;
+                background: var(--sh-blue);
+                position: absolute;
+                right: 0;
+                z-index: 1;
+                transform: translate(1vw, -1.5vw);
+            }
         }
     }
 </style>
