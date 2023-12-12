8fc879df8ac9db28c2c6fe5ac333e4b88cc7810e
lilskywalkr <ronald.frangulyan@gmail.com>
2023-12-12 14:00:51 +0100

    team section

---
+++ src/components/team/People.vue
@@ -0,0 +1,64 @@
+<script setup>
+    import { ref, defineProps, onMounted } from 'vue';
+    import { useI18n } from 'vue-i18n';
+
+
+    const props = defineProps(['imgArray', 'names', 'descs']);
+</script>
+
+<template>
+    {{ console.log(props.names) }}
+    <div class="people-container">
+        <div class="person-box" v-for="(img, index) in imgArray" :key="index">
+            <div class="person-img" v-bind:style="{backgroundImage: `url(${img})`}"></div>
+            <div class="person-desc">
+                <h1>{{ props.names[index] }}</h1>
+                <p>Co-Founder,<br>Key Account<br>Manager</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .people-container {
+        width: 100vw;
+        height: auto;
+        padding: 10vw 5vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-flow: row wrap;
+        gap: 5vw 10vw;
+
+        .person-box {
+            width: auto;
+            height: auto;
+            display: flex;
+            flex-flow: column;
+
+            .person-img {
+                width: 10vw;
+                height: 10vw;
+                border-radius: 10vw;
+                background-position: center;
+                background-size: contain;
+
+                background: white;
+            }
+
+            .person-desc {
+                color: var(--sh-white);
+                text-align: center;
+                padding: 1vw 0 0 0;
+
+                h1 {
+                    font-size: 1.5vw;
+                }
+
+                p {
+                    font-size: 1vw;
+                }
+            }
+        }
+    }
+</style>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,6 +1,7 @@
 <script setup>
     import Hero from '@/components/Hero.vue';
     import Footer from '@/components/Footer.vue';
+    import People from '@/components/team/People.vue';
     import { ref, onMounted } from 'vue';
     import { getImagesFromUnsplush } from '../components/modules/getImages';
     
@@ -12,12 +13,17 @@
     const heroContent = ref('')
 
     onMounted(async () => {
-        await getImagesFromUnsplush(1, imgArray.value);
+        await getImagesFromUnsplush(7, imgArray.value);
     });
+
+    // props for people (team members)
+    const names = ref(['Mateusz Milczarek', 'Filip Lecrut', 'Jakub Tutka', 'Paweł Rutkowski', 'Vitalii Shcherbyna', 'Piotr Jasiński', 'Jakub Sońta', 'Bartek Palmowski']);
+    const descs = ref([]);
 </script>
 
 <template>
-    <Hero :imgArray="imgArray"/>
+    <Hero :imgArray="imgArray" :header="$t('ourTeamHeroHeader')" :content="$t('ourTeamHeroContent')"/>
+    <People :imgArray="imgArray" :names="names" :desc="descs" />
 
     <Footer></Footer>
 </template>
