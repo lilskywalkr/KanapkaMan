660efaf0971fca34247864cd1f1613139076e5af
lilskywalkr <ronald.frangulyan@gmail.com>
2024-01-29 00:53:19 +0100

    logos

--- components/home/Canvas.vue
+++ components/home/Canvas.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 
 const canvas = ref(null)
+let ctx = null // Store canvas context
+let loadedImages = [] // Store loaded images
 
 // Array to hold information about each image
 const images = [
-  { src: '_nuxt/assets/floating_images/ruby.webp', x: 400, y: 300, angle: 0, speed: 0.3, directionX: 0, directionY: 0 },
-  { src: '_nuxt/assets/floating_images/flutter.webp', x: 400, y: 300, angle: 45, speed: 0.5, directionX: 0, directionY: 0 },
-  { src: '_nuxt/assets/floating_images/firebase.webp', x: 400, y: 300, angle: 15, speed: 0.7, directionX: 0, directionY: 0 },
-  { src: '_nuxt/assets/floating_images/vue.webp', x: 400, y: 300, angle: 45, speed: 0.1, directionX: 0, directionY: 0 },
-  { src: '_nuxt/assets/floating_images/swift.webp', x: 400, y: 300, angle: 45, speed: 0.9, directionX: 0, directionY: 0 },
-  { src: '_nuxt/assets/floating_images/mongo.webp', x: 400, y: 300, angle: 45, speed: 0.4, directionX: 0, directionY: 0 },
+  { src: '_nuxt/assets/floating_images/ruby.png', x: 500, y: 300, angle: 0, speed: 0.3, directionX: 0, directionY: 0 },
+  { src: '_nuxt/assets/floating_images/flutter.png', x: 500, y: 300, angle: 45, speed: 0.5, directionX: 0, directionY: 0 },
+  { src: '_nuxt/assets/floating_images/firebase.png', x: 500, y: 300, angle: 15, speed: 0.7, directionX: 0, directionY: 0 },
+  { src: '_nuxt/assets/floating_images/vue.png', x: 500, y: 300, angle: 45, speed: 0.1, directionX: 0, directionY: 0 },
+  { src: '_nuxt/assets/floating_images/swift.png', x: 500, y: 300, angle: 45, speed: 0.9, directionX: 0, directionY: 0 },
+  { src: '_nuxt/assets/floating_images/mongo.png', x: 500, y: 300, angle: 45, speed: 0.4, directionX: 0, directionY: 0 },
   // Add more images as needed
 ]
 
@@ -34,11 +36,11 @@ function randomDirection() {
 }
 
 async function run() {
-  const ctx = canvas.value.getContext('2d')
+  ctx = canvas.value.getContext('2d')
 
   // Load images
   const imagePromises = images.map(image => loadImage(image.src))
-  const loadedImages = await Promise.all(imagePromises)
+  loadedImages = await Promise.all(imagePromises)
 
   // Initialize random directions
   images.forEach((image) => {
@@ -47,33 +49,50 @@ async function run() {
   })
 
   // Start animation loop
-  function animate() {
-    ctx.clearRect(0, 0, canvas.value.width, canvas.value.height)
-
-    images.forEach((image, index) => {
-      // Move image
-      image.x += image.speed * image.directionX
-      image.y += image.speed * image.directionY
-
-      // Check boundaries and change direction
-      if (image.x < 0 || image.x > canvas.value.width)
-        image.directionX *= -1
-
-      if (image.y < 0 || image.y > canvas.value.height)
-        image.directionY *= -1
-
-      drawImage(ctx, loadedImages[index], image.x, image.y)
-    })
-
-    requestAnimationFrame(animate)
-  }
-
   animate()
+}
+
+function animate() {
+  // Clear canvas
+  ctx.clearRect(0, 0, canvas.value.width, canvas.value.height)
+
+  // Update canvas size
+  canvas.value.width = window.innerWidth
+  canvas.value.height = window.innerHeight
+
+  images.forEach((image, index) => {
+    // Move image
+    image.x += image.speed * image.directionX
+    image.y += image.speed * image.directionY
+
+    // Check boundaries and change direction
+    if (image.x < 0 || image.x > canvas.value.width)
+      image.directionX *= -1
+
+    if (image.y < 0 || image.y > canvas.value.height)
+      image.directionY *= -1
+
+    drawImage(ctx, loadedImages[index], image.x, image.y)
+  })
+
+  requestAnimationFrame(animate)
 }
 
 onMounted(() => {
   run()
 })
+
+// Listen for window resize event
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', handleResize)
+})
+
+function handleResize() {
+  canvas.value.width = window.innerWidth
+  canvas.value.height = window.innerHeight
+}
+
+window.addEventListener('resize', handleResize)
 </script>
 
 <template>
@@ -85,11 +104,9 @@ onMounted(() => {
 </template>
 
 <style scoped lang="scss">
-    canvas {
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      transform: translate(-50%, -50%);
-      z-index: -2;
-    }
+canvas {
+  position: absolute;
+  left: 0;
+  top: 0;
+}
 </style>
